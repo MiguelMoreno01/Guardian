fdae71b8fe9374dcffbc1de57c705609c312efd4
tonyjiao <tonyj2@illinois.edu>
2020-05-04 16:34:05 -0500

    hi

--- app/src/main/java/com/example/guardianapp/getLocation.java
+++ app/src/main/java/com/example/guardianapp/getLocation.java
@@ -4,12 +4,14 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
+import android.content.Intent;
 import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
 import android.text.Html;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -29,15 +31,17 @@ import java.util.Locale;
 
 public class getLocation extends AppCompatActivity {
 
-    Button btLocation;
+    Button location, confirmation;
     TextView textView1, textView2, textView3, textView4, textView5;
     FusedLocationProviderClient fusedLocationProviderClient;
+    private String state;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_location);
-        btLocation = findViewById(R.id.bt_location);
+        location = findViewById(R.id.findLocation);
         textView1 = findViewById(R.id.text_view1);
         textView2 = findViewById(R.id.text_view2);
         textView3 = findViewById(R.id.text_view3);
@@ -45,7 +49,7 @@ public class getLocation extends AppCompatActivity {
         textView5 = findViewById(R.id.text_view5);
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
 
-        btLocation.setOnClickListener(new View.OnClickListener() {
+        location.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (ActivityCompat.checkSelfPermission(getLocation.this,
@@ -59,6 +63,14 @@ public class getLocation extends AppCompatActivity {
                 }
             }
 
+        });
+        confirmation = (Button) findViewById(R.id.confirmation);
+        confirmation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //city = cityInput.getText().toString();
+                openDataofState();
+            }
         });
     }
 
@@ -88,8 +100,9 @@ public class getLocation extends AppCompatActivity {
                         ));
                         textView4.setText(Html.fromHtml(
                                 "<font color='#6200EE'><b>Locality :</b><br></font>"
-                                        + addresses.get(0).getLocality()
+                                        + addresses.get(0).getAdminArea()
                         ));
+                        state = addresses.get(0).getAdminArea();
                         textView5.setText(Html.fromHtml(
                                 "<font color='#6200EE'><b>Address :</b><br></font>"
                                         + addresses.get(0).getAddressLine(0)
@@ -100,8 +113,12 @@ public class getLocation extends AppCompatActivity {
                         e.printStackTrace();
                     }
                 }
-
             }
         });
+    }
+    public void openDataofState() {
+        Intent intent = new Intent(this, DataofState.class);
+        intent.putExtra("State", state);
+        startActivity(intent);
     }
 }
